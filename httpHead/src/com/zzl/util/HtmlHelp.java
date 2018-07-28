0021a28b9e63efac072a0a5fc15ada1e3c8373fc
mengxiangqihangz <zhiang082436>
2018-07-28 17:00:32 +0800

    整理

--- httpHead/src/com/zzl/util/HtmlHelp.java
+++
@@ -1,9 +0,0 @@
-package com.zzl.util;
-
-public class HtmlHelp {
-	public static String getHeadWithTitle(String title){
-		return "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n"
-				+ "<HTML>\n"
-				+ "<HEAD><TITLE>"+title+"</TITLE></HEAD>\n";
-	}
-}
